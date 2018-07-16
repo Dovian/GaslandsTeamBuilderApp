5da5003d265f31c9e572c2466fa611fefaad9d37
Daniel <deboerdovian@gmail.com>
2018-07-16 16:51:23 -0500

    Updated login config

--- Gaslands Team Builder/GaslandsTeamBuilder/GaslandsTeamBuilder/Startup.cs
+++ Gaslands Team Builder/GaslandsTeamBuilder/GaslandsTeamBuilder/Startup.cs
@@ -25,7 +25,7 @@ namespace GaslandsTeamBuilder
             app.UseCookieAuthentication(new CookieAuthenticationOptions
             {
                 AuthenticationType = DefaultAuthenticationTypes.ApplicationCookie,
-                LoginPath = new PathString("/Account/Login")
+                LoginPath = new PathString("/Account")
             });
 
             AntiForgeryConfig.UniqueClaimTypeIdentifier = ClaimTypes.NameIdentifier;
