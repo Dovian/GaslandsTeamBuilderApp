09de338287237ebda1f39652e70b1978e9e370fe
Daniel Deboer <DanielD@magenic.com>
2018-07-23 11:13:44 -0500

    Updated Error Handling
    Setup custom pages to handle 404 and default error pages

---
+++ Gaslands Team Builder/GaslandsTeamBuilder/GaslandsTeamBuilder/Controllers/ErrorController.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace GaslandsTeamBuilder.Controllers
+{
+    [AllowAnonymous]
+    public class ErrorController : BaseController
+    {
+        public ViewResult Index()
+        {
+            Response.StatusCode = 500;
+            return View("Error");
+        }
+        public ViewResult NotFound()
+        {
+            Response.StatusCode = 404;
+            return View("NotFound");
+        }
+    }
+}
